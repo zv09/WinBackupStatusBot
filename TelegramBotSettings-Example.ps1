585eda43066450c5405100487726f04a4330f85a
zv09 <7338823+zv09@users.noreply.github.com>
2021-08-10 21:48:45 +0300

    Update TelegramBotSettings-Example.ps1
    notes update

--- TelegramBotSettings-Example.ps1
+++ TelegramBotSettings-Example.ps1
@@ -1,7 +1,7 @@
 ﻿#Users ID starts as positive number and Groups ID as negative one
 
-$ChatID = "XXXXXXXXX" #Telegram user account
+$ChatID = "XXXXXXXXX" #Telegram user account for getting personal messages
 
-#$ChatID = "-XXXXXXXXX" #Telegram group account
+#$ChatID = "-XXXXXXXXX" #Telegram group account for distributed messages among Group participants 
 
 $TokenID = "YYYYYYYYYY:WWWWWWWWWWWWWWWWWWWWWWWWWWWWW-ZZZZZ"   #Telegram token given by the BotFather bot 
